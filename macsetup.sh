4299205da73b7c61f84d7c95fc17a507b80f4970
ahousseini <admin@admin-mac.local>
2019-03-26 19:16:52 +0100

    Update macsetup.sh

--- macsetup.sh
+++ macsetup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-  
+
   # Homebrew
-  
+
   if ! which brew > /dev/null; then
     ruby -e "$(curl -Ls 'https://github.com/Homebrew/install/raw/master/install')" < /dev/null > /dev/null 2>&1
   fi
-  
+
   brew analytics off
   brew update
   brew doctor
-  
+
   brew tap caskroom/cask
   brew tap caskroom/fonts
   brew tap homebrew/command-not-found
@@ -18,9 +18,9 @@
   brew tap homebrew/cask-fonts
   brew tap homebrew/cask-drivers
   brew tap jamf/tap
-  
+
   # Homebrew
-  
+
   formulae_install=(
     'vitorgalvao/tiny-scripts/cask-repair'
     'mas'
@@ -37,13 +37,13 @@
     'kubectl'
     'trash'
   )
-  
+
   for ((i = 0; i < "${#formulae_install[@]}"; i++)); do
     brew install ${formulae_install[$i]}
   done
-  
+
   # Applications
-  
+
   mas_install=(
     '466261145'  # Remotix VNC, RDP & NEAR
     '443987910'  # 1Password 6
@@ -54,17 +54,18 @@
     '689859502'  # Push Diagnostics
     '897243628'  # IPv4 Subnet
   )
-  
+
   for ((i = 0; i < "${#mas_install[@]}"; i++)); do
     mas install ${mas_install[$i]}
   done
-  
+
   sudo xcodebuild -license accept
-  
+
   cask_install=(
     'anydesk'
     'alfred'
     'autodmg'
+    'bbedit'
     'appcleaner'
     'blockblock'
     'caffeine'
@@ -126,31 +127,31 @@
     'quicklookase'
     'qlvideo'
   )
-  
+
   for ((i = 0; i < "${#cask_install[@]}"; i++)); do
     brew cask install --no-quarantine ${cask_install[$i]}
   done
-  
+
   # Install autopkg and add basic repos
-  
+
   autopkg_version="1.0.4"
-  
+
   cd "/private/tmp/" && curl -s -O -J -L "https://github.com/autopkg/autopkg/releases/download/v${autopkg_version}/autopkg-${autopkg_version}.pkg"
-  
+
   sudo installer -pkg "/private/tmp/autopkg-${autopkg_version}.pkg" -target /
   rm -rf "/private/tmp/autopkg-${autopkg_version}.pkg"
-  
+
   autopkg_repo=(
     "dataJAR-recipes"
     "prochat-recipes"
   )
-  
+
   for ((i = 0; i < "${#autopkg_repo[@]}"; i++)); do
     autopkg repo-add ${autopkg_repo[$i]}
   done
-  
+
   # Binaries Linking
-  
+
   chmod +x "/Users/admin/GitHub/aky-source/aky.sh" && ln -s "/Users/admin/GitHub/aky-source/aky.sh" "/usr/local/bin/aky"
   chmod +x "/Users/admin/GitHub/aky-source/rg/rg.sh" && ln -s "/Users/admin/GitHub/aky-source/rg/rg.sh" "/usr/local/bin/rg"
   chmod +x "/Users/admin/GitHub/aky-source/csc/csc.sh" && ln -s "/Users/admin/GitHub/aky-source/csc/csc.sh" "/usr/local/bin/csc"
@@ -166,14 +167,14 @@
   chmod +x "/Users/admin/GitHub/helper/bin/tmpDir" && ln -s "/Users/admin/GitHub/helper/bin/tmpDir" "/usr/local/bin/tmpDir"
   chmod +x "/Users/admin/GitHub/helper/bin/jamff" && ln -s "/Users/admin/GitHub/helper/bin/jamff" "/usr/local/bin/jamff"
   chmod +x "/Users/admin/GitHub/helper/bin/write-out-curl" && ln -s "/Users/admin/GitHub/helper/bin/write-out-curl" "/usr/local/bin/write-out-curl"
-  
+
   # git
-  
+
   git config --global core.excludesfile ~/.gitignore
   echo ".DS_Store" >> ~/.gitignore
-  
+
   # Finder
-  
+
   defaults write com.apple.finder "_FXShowPosixPathInTitle" -bool true
   defaults write com.apple.finder "NewWindowTarget" -string PfHm
   defaults write com.apple.finder "ShowHardDrivesOnDesktop" -bool true
@@ -189,18 +190,18 @@
   defaults write com.apple.finder "FXPreferredSearchViewStyle" -string clmv
   defaults write com.apple.finder "FXPreferredViewStyle" -string clmv
   defaults write com.apple.finder "SearchRecentsSavedViewStyle" -string clmv
-  
+
   osascript -e 'tell app "Finder" to quit'
-  
+
   # Dock
-  
+
   defaults write com.apple.dock orientation -string left
   defaults write com.apple.dock show-recents -bool false
-  
+
   dockutil --remove "all"
-  
+
   sleep 5
-  
+
   dock_items=(
     'Self Service Icon Maker.app'
     'Microsoft Excel.app'
@@ -240,29 +241,28 @@
     'Safari.app'
     'System Preferences.app'
   )
-  
+
   for ((i = 0; i < "${#dock_items[@]}"; i++)); do
     dockutil --add "/Applications/${dock_items[$i]}" --no-restart
   done
-  
+
   osascript -e 'tell app "Dock" to quit'
-  
+
   # Enable CUPS web interface
-  
+
   cupsctl WebInterface=yes
-  
+
   # Finder Sidebar
-  
+
   sidebar_items=(
     '/Users/admin/WebOffice'
     '/Users/admin/GitHub'
   )
-  
+
   for ((i = 0; i < "${#sidebar_items[@]}"; i++)); do
     /usr/bin/sfltool add-item com.apple.LSSharedFileList.FavoriteItems file://${sidebar_items[$i]}
   done
-  
+
   # Set UTI default handler
-  
+
   duti -s "com.chocolatapp.Chocolat" sh all
-  
